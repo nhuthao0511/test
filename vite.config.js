6d61586983656b6bc75fb1b64dbb4241cdd478a5
nhuthao0511 <haotran9871@gmail.com>
2026-04-18 18:52:49 +0700

    Fix white screen error

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/test/', // This must match your GitHub repo name exactly!
+  // Replace 'test' with your exact GitHub repository name
+  base: '/test/', 
 })
